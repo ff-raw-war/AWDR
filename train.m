7d7ed26f133e1d617516f2cde5b8f5a9d3fe2093
ff-raw-war <68218644+ff-raw-war@users.noreply.github.com>
2026-07-02 16:44:10 +0800

    Update train.m

--- train.m
+++ train.m
@@ -1,26 +1,26 @@
 function [B, A, Z] = train(X, c, alpha, ep1, ep2, epsilon, maxIter)
-    % tic
+    
     
     lossVec = [];
     iterVec = [];
     errVec = [];
     errVec2 = [];
 
-    rho = 1.01; % 原ADMM
-	% rho = 3; % L0LRSSC方法
-    mu = 0.1; % 原ADMM和L0LRSSC方法一样，都是0.1
-    maxMu = 10^5; % L0LRSSC方法是10^6， SSC是10^5
+    rho = 1.01; 
+	% rho = 3;
+    mu = 0.1; 
+    maxMu = 10^5; 
     threshold = 2*10^-4; 
     [d, n] = size(X);
-    dd = d; % 降维之后的维度
+    dd = d;
     %{
-    % 用VDA来初始化A
+    
     [anchor, ind2, score] = VDA(X', c);
-    % c×d, 选取下标, n×c
-    A = anchor'; % A是d×c
+   
+    A = anchor'; 
     %｝
     
-    % 对锚点画图
+    
     scatter(X(:,1), X(:,2), 'b.') % Blue dots
     hold on
     scatter(anchor(:,1), anchor(:,2), 'r+') % Red plus signs
@@ -44,7 +44,7 @@ function [B, A, Z] = train(X, c, alpha, ep1, ep2, epsilon, maxIter)
     err = 10*threshold; 
     errVec = [errVec err];
     loss = getLoss(Z);
-    lossVec = [lossVec ; loss]; % 记录每一次迭代的loss的值，从第0次开始
+    lossVec = [lossVec ; loss]; 
     iter = 1;
     iterVec = [iterVec iter];
 	notConverged = 1;
@@ -64,30 +64,30 @@ function [B, A, Z] = train(X, c, alpha, ep1, ep2, epsilon, maxIter)
         A = getA(X, B, Z, J, mu);
         % tic;
 
-        % mu = ALF; % SSC的mu的算法，是固定值
-        mu = min(rho*mu, maxMu); % GMC-LRSSC方法
+        % mu = ALF; 
+        mu = min(rho*mu, maxMu); 
         
-        % 终止迭代条件为约束项
+        
         err = norm(Z'*es1-es2, 'fro');
 		% err = max(max(abs(P'*X-P'*X*C-E)));
 
 		loss = getLoss(Z);
         errVec = [errVec err];
         lossVec = [lossVec loss];
-        iterVec = [iterVec iter]; % 记录迭代次数，从第0此开始
-		% 检查是否收敛
+        iterVec = [iterVec iter];
+		
         
 		if iter >= maxIter
 			notConverged = 0;
 		end
         
-        % 约束项收敛
+        
 		if err<threshold
 			notConverged = 0;
 		end
         
 		%{
-        % 终止迭代条件为loss值差
+        
         if ((loss0-loss)'*(loss0-loss) < epsilon) || (loss > loss0)
             notConverged = 0;
         end % 
@@ -97,7 +97,7 @@ function [B, A, Z] = train(X, c, alpha, ep1, ep2, epsilon, maxIter)
     end % while iter
     
     %{
-    % 画图用
+    
     %plot(hengzhou, zongzhou, 'd-');
     if iter < 10
         for i = 1:10-iter
